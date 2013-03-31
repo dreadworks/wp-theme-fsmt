daa4a60ce663d1615e70956184fd5676c5eff288
Felix Hamann <dev@dreadworks.de>
2013-04-01 01:57:12 +0200

    first markup

--- src/php/header.php
+++ src/php/header.php
@@ -1,3 +1,19 @@
-<div id="header">
-	<img href="img/bg-header.jpg" alt="" />
-</div>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>" />
+		<title><?php echo wp_title(); ?></title>
+		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+	</head>
+	<body>
+
+		<div id="wrapper">
+			<header>
+				<h1><?php bloginfo('name'); ?></h1>
+				<section class="decoration">
+					<span></span>
+					<h2><?php bloginfo('description'); ?></h2>
+				</section>
+			</header>
+
+				
