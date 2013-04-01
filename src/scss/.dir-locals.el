2ee029fdcb702db90e541a07605064679af2dc3b
Felix Hamann <dev@dreadworks.de>
2013-04-01 13:41:22 +0200

    added emacs scss-mode configuration for convenience

---
+++ src/scss/.dir-locals.el
@@ -0,0 +1,7 @@
+;;
+;;  emacs scss-mode options
+;;    https://github.com/antonj/scss-mode
+;;    https://github.com/antonj/scss-mode/pull/9
+
+((scss-mode
+  (scss-output-directory . "../../build/")))
